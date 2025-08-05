1d3254860c411bc3a0d1e7f8a3014936a7d32c3f
Polinaя <rybalkopr@gmail.com>
2025-08-05 05:37:39 -0400

    Sprint 2 final project files

--- src/main/java/model/Apple.java
+++ src/main/java/model/Apple.java
@@ -25,3 +25,4 @@ public class Apple extends Food {
         }
     }
 }
+
--- src/main/java/model/constants/Discount.java
+++ src/main/java/model/constants/Discount.java
@@ -3,3 +3,4 @@ package model.constants;
 public class Discount {
     public static final double RED_APPLE_DISCOUNT = 60.0;
 }
+
